117a7ae5714be3b3f4c216b1faee49e5b659b4f0
fragged-up <154810298+fragged-up@users.noreply.github.com>
2025-04-26 07:10:45 +0300

    sync backend + cors origin

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
-  app.enableCors({ origin: 'http://localhost:5173'});
+  app.enableCors({
+    origin: configService.get<string>('CORS_ORIGIN') || '*',
+  });
+
 
   app.useGlobalPipes(new ValidationPipe());
 
